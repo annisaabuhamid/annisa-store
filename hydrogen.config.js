ff281a02f7d8d03992ba8a4f9a7aa8bca89ab6e7
annisaabuhamid <annisaabuhamid@gmail.com>
2022-08-10 17:13:06 +0800

    connect to shop

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,10 +2,10 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
-    // storeDomain: 'editions-demo.myshopify.com',
-    // storefrontToken: 'f18c5a628794c4d3eb0340327abdb19e',
+    // storeDomain: 'hydrogen-preview.myshopify.com',
+    // storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'editions-demo.myshopify.com',
+    storefrontToken: 'f18c5a628794c4d3eb0340327abdb19e',
     storefrontApiVersion: '2022-07',
   },
 });
